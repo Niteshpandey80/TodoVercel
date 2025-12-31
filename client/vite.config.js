95a8683c49a1e6a8f340ab7aede39e4c6853d647
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-12-31 13:46:34 +0530

    fir chnage

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true, // automatically opens in browser on npm run dev
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // allows import like "@/pages/Signup.jsx"
-    },
-  },
-});
+  plugins: [tailwindcss(),
+react()],
+})
